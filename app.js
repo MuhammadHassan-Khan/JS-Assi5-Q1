8282438a5f7bc8e75f36ab71d3ff81fff431e9c7
Muhammad Hassan Khan <138271415+hassanaleem125@users.noreply.github.com>
2023-11-20 15:23:39 +0500

    js Assi5/Q1

---
+++ app.js
@@ -0,0 +1,8 @@
+document.write("<br>")
+var myvar = 5
+
+document.write("Initial value: 5"+ "<br>")
+document.write("Value after increment is: 6"+ "<br>")
+document.write("Value after addition is: 13"+ "<br>")
+document.write("Value after decrement is: 12"+ "<br>")
+document.write("The remainder is : 0"+ "<br>")
